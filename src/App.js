37b8c5acac5de79d5e50205297ae8576aa738bdc
OsmarSoujirou <osmar.lpc.net@gmail.com>
2021-05-26 20:33:48 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import BookCard from 'components/Books/Card-01/Card';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Books/Card-01/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BooksCard = () =>{
+	<div> HI</div>
+}
+
+export default BooksCard;
