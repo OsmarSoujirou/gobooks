db2009ccb10568f3447edd1bdd0d19a66a2792d8
OsmarSoujirou <osmar.lpc.net@gmail.com>
2021-05-27 22:14:44 -0300

    Teste Final.

--- src/components/FavObj.js
+++ src/components/FavObj.js
@@ -1,24 +1,13 @@
 import React, {Component} from 'react';
 
 class FavObj extends Component {
-    constructor(){
-        super();
-        this.state = {
-            clicked: false
-        }
-    }
-
-    changeColor(){
-        this.setState({clicked: !this.state.clicked})
-    }
 
     render(){
-        var btn_class = this.state.clicked ? "fill-star fas fa-star" : "void-star fas fa-star";
-
+        
         return (
-             
-                <i className={btn_class} onClick={this.changeColor.bind(this)}>FAVORITO</i>
-             
+             <div className={this.props.favorito} >
+                <a>FAVORITO</a>
+             </div>
         )
     }
 }
